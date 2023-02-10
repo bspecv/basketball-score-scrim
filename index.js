0a84c57dbb9ed2242aebf11e79ef8f8dab610665
bspecv <farthingshane@gmail.com>
2023-02-09 22:11:38 -0500

    1st commit

---
+++ index.js
@@ -0,0 +1,30 @@
+let count = 0
+let guestCount = 0
+
+let homeScoreDigits = document.getElementById("home-score-digits")
+
+let guestScoreDigits = document.getElementById("guest-score-digits")
+console.log(guestScoreDigits)
+
+
+function hPoint1 () {
+    let result = count 
+homeScoreDigits.textContent = count +=1
+}
+function hPoint2 () {
+homeScoreDigits.textContent = count += 2
+}
+function hPoint3 () {
+homeScoreDigits.textContent = count += 3
+}
+function gPoint1 () {
+guestScoreDigits.textContent = guestCount +=1
+}
+
+function gPoint2 () {
+guestScoreDigits.textContent = guestCount += 2
+}
+function gPoint3 () {
+guestScoreDigits.textContent = guestCount += 3
+}
+
